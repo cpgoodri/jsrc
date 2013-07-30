976be2b8c0c30ff6c451b3a0e388f0617cfc367b
Carl Goodrich <carlpgoodrich@gmail.com>
2013-07-30 13:52:40 -0400

    Changed to netcdf4 c++ api

--- test2.cpp
+++ test2.cpp
@@ -7,9 +7,13 @@
 #include "Computers/StaticComputer.h"
 #include "Computers/MatrixInterface.h"
 #include "Minimization/minimizer.h"
-using namespace std;
+#include <netcdf>
+
 using namespace LiuJamming;
 #define DIM 2
+using namespace std;
+using namespace netCDF;
+using namespace netCDF::exceptions;
 
 void test1(int N, dbl phi, int seed)
 {
@@ -49,8 +53,21 @@ void test1(int N, dbl phi, int seed)
 	CStaticComputer<DIM> c(s);
 	CSimpleMinimizer<DIM> miner(c, CSimpleMinimizer<DIM>::FIRE);
 	
-	CStaticComputer<DIM> c2(s2);
-	CSimpleMinimizer<DIM> miner2(c2, CSimpleMinimizer<DIM>::FIRE);
+//	CStaticComputer<DIM> c2(s2);
+//	CSimpleMinimizer<DIM> miner2(c2, CSimpleMinimizer<DIM>::FIRE);
+
+try{
+	string fn = "test.nc";
+	NcFile outFile(fn, NcFile::replace);
+	NcDim recDim = outFile.addDim("rec");
+	NcVar myVar = outFile.addVar("testVar", ncDouble, recDim);
+}
+catch (NcException& e)
+{
+	cout << "unknown error"<<endl;
+	e.what();
+}
+
 
 	/*
 	//Create the system
